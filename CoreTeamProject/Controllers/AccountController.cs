9aa65736386b463b4d36730c9524400ad23f857a
Leanne Williston <Leanne.williston@gmail.com>
2017-05-17 15:34:38 -0300

    Worked on Log In

--- CoreTeamProject/Controllers/AccountController.cs
+++ CoreTeamProject/Controllers/AccountController.cs
@@ -89,7 +89,8 @@ namespace CoreTeamProject.Controllers
             }
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return RedirectToLocal(returnUrl);
+           
         }
 
         //
